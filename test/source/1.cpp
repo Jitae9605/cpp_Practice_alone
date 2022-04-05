8c6fc2346f61491ced9b66e9d773a03fde6f45cf
Jitae9605 <dlwlxo3819@naver.com>
2022-04-05 14:46:09 +0900

    class practice  and  test about make repository

--- test/source/1.cpp
+++ test/source/1.cpp
@@ -15,7 +15,35 @@ public:
 	a1111(int aa) :a(aa)
 	{ }
 
-	int geta() { return a; }
+	virtual int geta() { return a; }
+
+	void print_a()
+	{
+		cout << "a : " << a;
+	}
+};
+
+class b1111:public a1111
+{
+private:
+	int b1;
+	int b2;
+public:
+	b1111(int ab1, int ab2):a1111(ab1),b2(ab2)
+	{ }
+
+	int getb() { return b2; }
+
+	void print_b()
+	{
+		print_a();
+		cout << "b : " << b2;
+	}
+
+
+	
+
+
 };
 
 int main(void)
