5c64cef5627e780ea259619389fbf480545bfa8b
Jitae9605 <dlwlxo3819@naver.com>
2022-04-13 16:10:44 +0900

    필기,쿼리 추가 저장

--- test/쿼리 임시저장/커서.sql
+++ test/쿼리 임시저장/커서.sql
@@ -30,3 +30,20 @@ print '고객 키의 평균 ==> ' + cast(@totalheight/@cnt as char(10))
 close userTbl_cursor; -- 커서닫음
 
 deallocate userTbl_cursor; -- 커서할당헤제
+
+
+-- global = 전역
+-- local = 지역
+
+-- forward_only = 시작 행부터 끝 행까지 단방향으로만 커서가 움직임
+-- scroll = 커서가 자유롭게 이동 ( fetch next/first/last/prior... )
+
+-- static = 커서가 테이블을 복사해서 가져옴(커서 open이후의 update/insert 반영X) 
+-- keyset = 커서가 대상의 키값들만 다져옴(커서 open이후의 update는 반영O, insert는 반영X)
+-- dynamic = 커서가 테이블의 키값을 가져옴(커서open이후의 모든 변경사항 반영O), (디폴트 설정)
+-- fast_forward = 커서에서 행데이터 수정없으면 가장 뛰어난 성능
+-- 성능 = fast_forward > static > keyset > dynamic
+
+-- read_only = 읽기전용
+-- scroll_locks = 위치지정 업데이트/삭제 허용
+-- optimistic = 커서open이후 원본의 수정이 커서의 데이터에 반영되지 않게 설정
